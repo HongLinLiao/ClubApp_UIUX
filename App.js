68e4bd68630797d0357c8012d2d9cda61d3dc547
LeoLiao <honglin0822@outlook.com>
2018-07-11 21:29:39 +0800

    test_init

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import HomePage from './test/HomePage.js'
+// import SearchPage from './test/SearchPage.js'
+// import ClubPage from './test/ClubPage.js'
+// import ClubCalendarPage from './test/ClubCalendarPage.js'
+// import ClubStoriesPage from './test/ClubStoriesPage.js'
+// import PostsPage from './test/PostsPage.js'
+// import ActiviesPage from './test/ActiviesPage.js'
+// import CalendarPage from './test/CalendarPage.js'
+// import PersonalPage from './test/PersonalPage.js'
+// import PersonalPWDPage from './test/PersonalPWDPage.js'
+// import PersonalMailPage from './test/PersonalMailPage.js'
+// import PersonalGroupPage from './test/PersonalGroupPage.js'
+// import SignInPage from './test/SignInPage.js'
+// import SignUpPage from './test/SignUpPage.js'
+// import SignMailPage from './test/SignMailPage.js'
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      // <HomePage />
+      // <SearchPage />
+      // <ClubPage />
+      // <ClubCalendarPage />
+      // <ClubStoriesPage />
+      // <PostsPage />
+      // <ActiviesPage />
+      // <CalendarPage />
+      // <PersonalPage />
+      // <PersonalPWDPage />
+      // <PersonalMailPage />
+      // <PersonalGroupPage />
+      // <SignInPage />
+      // <SignUpPage />
+      // <SignMailPage />
     );
   }
 }
